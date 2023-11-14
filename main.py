3c49e323f44070829a2f883d0889e851347be196
Zoltán Berkula <64430345+zoltanberkula@users.noreply.github.com>
2023-11-14 20:14:54 +0100

    aws connection init

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import jwt
 from fastapi import FastAPI, Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from tortoise import fields
@@ -6,10 +5,50 @@ from passlib.hash import bcrypt
 from tortoise.contrib.fastapi import register_tortoise
 from tortoise.contrib.pydantic import pydantic_model_creator
 from tortoise.models import Model
+import jwt
+import boto3
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel, Field
+import uuid
+from typing import List
+import os
+import time
+from dotenv import load_dotenv
+
+load_dotenv()
+
+aws_acc_key_id = os.environ.get("AWS_ACCESS_KEY_ID")
+aws_acc_key = os.environ.get("AWS_ACCESS_KEY")
+aws_db_service_id = os.environ.get("AWS_DB_SERVICE_ID")
+aws_db_service_region = os.environ.get("AWS_DB_SERVICE_REGION")
+aws_db_table_name = os.environ.get("AWS_DB_TABLE_NAME")
+
+fast_api_origin_addr = os.environ.get("FASTAPI_ORIGIN_ADDRESS")
+fast_api_token_url = os.environ.get("FASTAPI_TOKEN_URL")
+fast_api_token_type = os.environ.get("FASTAPI_TOKEN_TYPE")
+fast_api_jwt_secret = os.environ.get("FASTAPI_JWT_SECRET")
+
+sqlite_db_url = os.environ.get("SQLITE_DB_URL")
+
 
 app = FastAPI()
 
-JWT_SECRET = "myjwtsecret"
+dynamodb = boto3.resource(service_name=aws_db_service_id,
+                          region_name=aws_db_service_region,
+                          aws_access_key_id=aws_acc_key_id,
+                          aws_secret_access_key=aws_acc_key)
+
+origins = [
+    fast_api_origin_addr,
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 class User(Model):
     id = fields.IntField(pk=True)
@@ -46,8 +85,11 @@ async def generate_token(form_data: OAuth2PasswordRequestForm = Depends()):
                             )
     
     user_obj = await User_Pydantic.from_tortoise_orm(user)
-    token = jwt.encode(user_obj.dict(), JWT_SECRET)
-    return {'access_token' : token, 'token_type' : 'bearer'}
+    token = jwt.encode(user_obj.dict(), fast_api_jwt_secret)
+    return {
+            'access_token' : token,
+            'token_type' : fast_api_token_type
+            }
 
 
 @app.post('/users', response_model=User_Pydantic)
@@ -58,7 +100,7 @@ async def create_user(user: UserIn_Pydantic):
 
 async def get_current_user(token: str = Depends(oauth2_scheme)):
     try:
-        payload = jwt.decode(token, JWT_SECRET, algorithms=["HS256"])
+        payload = jwt.decode(token, fast_api_jwt_secret, algorithms=["HS256"])
         user = await User.get(id=payload.get('id'))
     except:
         raise HTTPException(
@@ -72,10 +114,33 @@ async def get_current_user(token: str = Depends(oauth2_scheme)):
 async def get_user(user: User_Pydantic = Depends(get_current_user)):
     return user
 
+@app.get('/')
+def read_root():
+    return "Hello Homepage!"
+
+@app.post('/submitdata')
+async def submitdata(data:dict):
+    table = dynamodb.Table(aws_db_table_name)
+
+    item= {
+        'bookID' : str(uuid.uuid4()),
+        'name' : data['name'],
+        'author': data['author']
+    }
+    table.put_item(Item= item)
+    #print(data)
+    return "Data submitted successfully"
+
+@app.get('/getAllBooks')
+def getall():
+    table = dynamodb.Table(aws_db_table_name)
+    items = table.scan()
+    #print(items)
+    return items
 
 register_tortoise(
     app,
-    db_url='sqlite://db.sqlite3',
+    db_url=sqlite_db_url,
     modules={'models' : ['main']},
     generate_schemas=True,
     add_exception_handlers=True
