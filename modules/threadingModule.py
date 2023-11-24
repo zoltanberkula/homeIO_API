280ca08750c92197932ccb26a212bc0dd48e00ca
Zoltán Berkula <64430345+zoltanberkula@users.noreply.github.com>
2023-11-24 01:42:49 +0100

    Update threadingModule.py

--- modules/threadingModule.py
+++ modules/threadingModule.py
@@ -30,24 +30,28 @@ def checkMainTHRD(inspectorVAR: Any, threshold: Any, identifier: str):
 
 def main():
     tobeInspected: int = 0
-    threshold = 672
+    threshold = 336
     tobeInspected2: int = 0
-    threshold2 = 1344
+    threshold2 = 672
+    tobeInspected3: int = 0
+    threshold3 = 1008
     print('**** Create a Thread with a function without any arguments ****')
 
     # Create a Thread with a function without any arguments
     th = threading.Thread(target=checkMainTHRD, args=(tobeInspected, threshold, "thread1 running"))
     th2 = threading.Thread(target=checkMainTHRD, args=(tobeInspected2, threshold2, "thread2 running"))
+    th3 = threading.Thread(target=checkMainTHRD, args=(tobeInspected3, threshold3, "thread3 running"))
 
     # Start the thread
     th.start()
     th2.start()
+    th3.start()
 
     # Print some messages on console
     for i in range(4):
        print('Hi from Main Thread')
        #time.sleep(0.5)
-       threshold = threshold + 1
+       #threshold = threshold + 1
 
     # Wait for thread to finish
     th.join()
